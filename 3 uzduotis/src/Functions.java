4a967b0c6c8af77a417e770bddedd3bd294fdbd2
Mindaugas <109964675+Kameleonas@users.noreply.github.com>
2022-12-30 15:09:04 +0200

    completed 3rd task

--- 3 uzduotis/src/Functions.java
+++ 3 uzduotis/src/Functions.java
@@ -45,10 +45,10 @@ public class Functions {
                     userInput = 0;
                 }
                 case 3 -> {// pajamų / išlaidų įrašo suradimas
-                    rastiIrasa(scanner, biudzetas);
+                    rastiRedaguotiIrasa(scanner, biudzetas, "rasti");
 //                      go to start of the program
-                        userInput = 0;
-                    }
+                    userInput = 0;
+                }
                 case 4 -> {
                     System.out.printf("Jūsų biudžetas: %s Eur\n", biudzetas.balansas(biudzetas.irasas));
                     userInput = 0;
@@ -68,8 +68,10 @@ public class Functions {
 ////                go to start of the program
                     userInput = 0;
                 }
-                case 8 -> {
-                    System.out.println("redaguoti irasa");
+                case 8 -> {// Įrašo redagavimas
+                    rastiRedaguotiIrasa(scanner, biudzetas, "redaguoti");
+//                    go to start of the program
+                    userInput = 0;
                 }
                 case 9 -> runProgram = false;
             }
@@ -179,34 +181,76 @@ public class Functions {
         }
     }
 
-    public static void rastiIrasa(Scanner scanner, Biudzetas biudzetas){
-        System.out.println("Įveskite įrašo kurį norite rasti ID:");
+    public static void rastiRedaguotiIrasa(Scanner scanner, Biudzetas biudzetas, String rasti) {
+        boolean replace = false;
+        Irasas toReplace = null;
+
+        if (rasti.equals("rasti")) {
+            System.out.println("Įveskite įrašo kurį norite rasti ID:");
+        } else if (rasti.equals("redaguoti")) {
+            System.out.println("Įveskite įrašo kurį norite redaguoti ID:");
+        }
         String userInFindByKey = scanner.next();
-        for (int i = 0; i < biudzetas.irasas.size(); i++) {
-            if (biudzetas.irasas.get(i).getId().equals(userInFindByKey)) {
-                System.out.printf("Įrašas rastas: \n %s \n", biudzetas.irasas.get(i));
+        for (Irasas element : biudzetas.irasas) {
+            if (element.getId().equals(userInFindByKey)) {
+                System.out.printf("Įrašas rastas: \n %s \n", element);
+                toReplace = element;
+            }
+        }
+        if (rasti.equals("redaguoti")) {
+            System.out.println("Suma: " + toReplace.getSuma());
+            if(userChoice(replace, scanner)) {
+                System.out.println("iveskite sumą: ");
+                double sum = scanner.nextDouble();
+                toReplace.setSuma(sum);
+            }
+            System.out.println("Data: " + toReplace.getDateTime());
+            if (userChoice(replace, scanner)){
+                System.out.println("įveskite datą: ");
+                String date = scanner.next();
+                toReplace.setDateTime(date);
+            }
+            System.out.println("Kategorijos indeksas: " + toReplace.getKategorijosIndeksas());
+            if (userChoice(replace, scanner)){
+                System.out.println("nauja kategorijos indekso vertė [1-2]: ");
+                int index = scanner.nextInt();
+                toReplace.setKategorijosIndeksas(index);
+            }
+            System.out.println("Papildoma informacija: " + toReplace.getPapildomaInfo());
+            if (userChoice(replace, scanner)){
+                System.out.println("Įveskite papildomą informaciją: ");
+                String info = scanner.next();
+                toReplace.setPapildomaInfo(info);
+            }
+            if (toReplace instanceof PajamuIrasas){
+                System.out.println("Sandoris užbaigtas: " + ((PajamuIrasas) toReplace).isTransferComplete());
+                if (userChoice(replace, scanner)){
+                    System.out.println("Įveskite ar sandoris užbaigtas [true / false]: ");
+                    boolean complete = scanner.nextBoolean();
+                    ((PajamuIrasas) toReplace).setTransferComplete(complete);
+                }
+            }
+            if (toReplace instanceof IslaiduIrasas){
+                System.out.println("Kortelė kuria buvo atsiskaityta: " + ((IslaiduIrasas) toReplace).getKortele());
+                if (userChoice(replace, scanner)){
+                    System.out.println("Įveskite kokia kortele buvo atsiskaityta: ");
+                    String card = scanner.next();
+                    ((IslaiduIrasas) toReplace).setKortele(card);
+                }
             }
         }
     }
 
-    public static void redaguotiIrasa(Scanner scanner, ArrayList<Irasas> irasas){
-        System.out.println("Įveskite ID įrašo, kurį norite redaguoti:");
-        String userInID = scanner.next();
-        for (int i = 0; i < irasas.size(); i++) {
-            if (irasas.get(i).getId().equals(userInID)){
-                System.out.println("Suma:" + irasas.get(i).getSuma());
-                System.out.println("""
-                [1] - koreguoti;
-                [2] - toliau""");
-                int userIn = scanner.nextInt();
-                if (userIn == 1){
-                    System.out.println("Įveskite sumą:");
-                    double suma = scanner.nextDouble();
-                    irasas.get(i).setSuma(suma);
-                }
-            }
-
-
+    public static boolean userChoice(boolean replace, Scanner scanner) {
+        System.out.println("""
+                [1] - keisti;
+                [2] - toliau;""");
+        int userInput = scanner.nextInt();
+        if (userInput == 1) {
+            return replace = true;
+        } else if (userInput == 2) {
+            return replace = false;
         }
+        return replace;
     }
 }
